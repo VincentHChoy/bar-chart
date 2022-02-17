92c884528fc74051cd6fc1edc28214bec328d2f9
VincentHChoy <vhchoy@ualberta.ca>
2022-02-17 02:47:00 +0000

    created scaling function for data input

--- my-scripts.js
+++ my-scripts.js
@@ -1,6 +1,23 @@
-$(document).ready(function(){
-  $("button").click(function(){
+var data = [0, 1, 2, 3, 4]
+
+$(document).ready(function () {
+  $("button").click(function () {
     $("p").css("background-color", "yellow");
   });
+
+ var scaledData = function barPercent(data) {
+    var newArray = [];
+    var dataLength = data.length;
+    data.forEach(element => {
+      newArray.push(element / dataLength)
+
+    }); return newArray
+  }
+
+  function dataBarChart(scaledData, options, element) {
+  
+  };
+  dataBarChart(scaledData,'dog','cat')
 });
+
 console.log('dog')
