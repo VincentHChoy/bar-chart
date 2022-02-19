ba1701c80ac0efbbe01eeb53592e821a4f330f11
VincentHChoy <vhchoy@ualberta.ca>
2022-02-19 22:25:44 +0000

    added function to change the titles on bars and can muniplulate bar length with input numbers

--- my-scripts.js
+++ my-scripts.js
@@ -1,23 +1,47 @@
-var data = [0, 1, 2, 3, 4]
+var inputData = [0, 10, 1, 3]
 
 $(document).ready(function () {
-  $("button").click(function () {
+  /*$("button").click(function () {
     $("p").css("background-color", "yellow");
-  });
+  });*/
 
- var scaledData = function barPercent(data) {
+
+  sumArray = function(data){
+    var sum = 0;
+    for (let i = 0; i < data.length; i++) {
+      sum += data[i];
+    } return sum
+  }
+
+
+  scaledArray = function barPercent(data) {
     var newArray = [];
     var dataLength = data.length;
     data.forEach(element => {
-      newArray.push(element / dataLength)
+      newArray.push((element / sumArray(data)) * 100)
 
     }); return newArray
   }
+function barTitles(inputData){
+  for (let i = 0; i < inputData.length; i++) {
+    console.log("td#item"+(i+1))
+    let element = $("td#item"+(i+1))                     //finds the element with the id
+    let textToReplace = element.text()              //selects the text of the element
+    let newText = textToReplace.replace("0",inputData[i])  // choose what to replace the text with
+    element.text(newText);
+    
+  }
+}
+barTitles(inputData)
 
-  function dataBarChart(scaledData, options, element) {
-  
+
+  function dataBarChart(scaledData/*, options, element*/) {
+    for (let i = 0; i < scaledData.length; i++) {
+      console.log(i + 1)
+      $(".item" + (i + 1) + "").css({ "background-color": "blue", "height": "" + scaledData[i] + "%" });
+
+
+    }
   };
-  dataBarChart(scaledData,'dog','cat')
+  dataBarChart(scaledArray(inputData))
 });
-
-console.log('dog')
