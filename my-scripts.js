6b643c049bdb30d97c0ea0f5c98bf711c69e1d00
VincentHChoy <vhchoy@ualberta.ca>
2022-02-21 19:56:37 +0000

    generates bars based on length of inputArray

--- my-scripts.js
+++ my-scripts.js
@@ -1,4 +1,4 @@
-var inputData = [0, 10, 1, 3]
+var inputData = [45, 10, 1, 3, 6, 7,15]
 
 $(document).ready(function () {
   /*$("button").click(function () {
@@ -6,7 +6,7 @@ $(document).ready(function () {
   });*/
 
 
-  sumArray = function(data){
+  sumArray = function (data) {
     var sum = 0;
     for (let i = 0; i < data.length; i++) {
       sum += data[i];
@@ -22,26 +22,38 @@ $(document).ready(function () {
 
     }); return newArray
   }
-function barTitles(inputData){
-  for (let i = 0; i < inputData.length; i++) {
-    console.log("td#item"+(i+1))
-    let element = $("td#item"+(i+1))                     //finds the element with the id
-    let textToReplace = element.text()              //selects the text of the element
-    let newText = textToReplace.replace("0",inputData[i])  // choose what to replace the text with
-    element.text(newText);
-    
+
+  // changes the text of the bars to the corresponding value
+  function barTitles(inputData) {
+    for (let i = 0; i < inputData.length; i++) {
+      console.log(inputData[i])
+      let element = $("td#item" + (i + 1))                      //finds the element with the id
+      let textToReplace = element.text()                       //selects the text of the element
+      let newText = textToReplace.replace("0", inputData[i])  // choose what to replace the text with
+      element.text(newText);
+
+    }
   }
-}
-barTitles(inputData)
+
+  // adds based on the number of bars
+  function addNewBars(number) {
+    for (let i = 1; i <= number; i++) {
+      console.log(i)
+      $("#bars").append("<tr class=\"border item" + (i) + "\"></tr>");
+      $(".item" + i).append("<td id=\"item" + (i) + "\">" + 0 + "</td>");
+      $("#item" + i).append("</tr>");
+
+    }
+  }
 
 
   function dataBarChart(scaledData/*, options, element*/) {
+    addNewBars(scaledData.length)
     for (let i = 0; i < scaledData.length; i++) {
-      console.log(i + 1)
       $(".item" + (i + 1) + "").css({ "background-color": "blue", "height": "" + scaledData[i] + "%" });
-
 
     }
   };
   dataBarChart(scaledArray(inputData))
+  barTitles(inputData)
 });
